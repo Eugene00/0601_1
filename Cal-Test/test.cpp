1a6752310d974c8795cb362d4f9579f59ec8b10e
yongjin lee <vir2048@gmail.com>
2023-06-01 10:10:10 +0900

    Add getdevide

--- Cal-Test/test.cpp
+++ Cal-Test/test.cpp
@@ -4,4 +4,6 @@
 TEST(TestCaseName, TestName) {
 	Cal* cal = new Cal();
 	EXPECT_TRUE(cal != nullptr);
+
+	EXPECT_EQ(3, cal->getdevide(9, 3));
 }
--- Cal/cal.cpp
+++ Cal/cal.cpp
@@ -1,4 +1,13 @@
 class Cal
 {
-	
+public:
+	int getdevide(int a, int b);
 };
+
+int Cal::getdevide(int a, int b)
+{
+	if (!b)
+		return 0;
+
+	return a / b;
+}
